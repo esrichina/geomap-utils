2c690a90c8de2ead9b49bd5d205300e5b042fc4f
lihubadboy <lihubadboy@163.com>
2018-12-12 15:10:11 +0800

    新功能

--- dist/geomap-utils.js
+++ dist/geomap-utils.js
@@ -9,12 +9,109 @@
   (global.GeomapUtils = factory());
 }(this, (function () { 'use strict';
 
-  var mapViewUtil = {};
+  /**
+   * 根据图层的title获取图层
+   * @author  lee  20181209
+   * @param {object} view  场景
+   * @param {string} title  名称
+   */
+  function getLayerByTitle(view, title) {
+    var foundLayer = view.map.layers.find(function (lyr) {
+      return lyr.title === title;
+    });
+    return foundLayer;
+  }
+  /**
+   * @summary 根据图层的索引获取图层
+   * @description 适用范围：二、三维
+   * @author  lee  20181209
+   * @param {*} view  场景
+   * @param {*} index  图层索引
+   */
+  function getLayerByIndex(view, index) {
+    var foundLayer = view.map.layers.getItemAt(index);
+    return foundLayer;
+  }
+  /**
+   * @summary 根据图层的id获取图层
+   * @author  lee  20181209
+   * @param {*} view  场景
+   * @param {*} id  图层id
+   */
+  function getLayerById(view, index) {
+    var foundLayer = view.map.findLayerById();
+    return foundLayer;
+  }
+  /**
+   * 根据图层名称，控制图层显隐藏
+   * @author  lee  20181208
+   * @param {*} view  场景
+   * @param {*} title  名称
+   * @param {*} visible 显示/隐藏  true or false
+   */
+  function setLayerVisible(view, title, visible) {
+    var foundLayer = getLayerByTitle(view, title);
+    foundLayer.visible = visible;
+  }
+  /**
+   * 根据要素的ObjectId高亮
+   * @author  lee  20181208
+   * @param {*} view  场景
+   * @param {*} title  名称
+   * @param {*} objectid 高亮要素的objectid
+   */
+  function highlightByLayerObjid(view, title, objectid) {
+    var foundLayer = getLayerByTitle(view, title);
+    view.whenLayerView(foundLayer).then(function (view) {
+      view.highlight(objectid * 1);
+    });
+  }
+  /**
+   * 根据条件过滤要素图层中符合条件的要素
+   * @author  lee  20181209
+   * @param {*} layer  图层
+   * @param {*} queryWhere  查询条件
+   */
+  function queryFeathersFromLayer(layer, queryWhere) {
+    var queryString = layer.createQuery();
+    queryString.where = queryWhere;
+    return layer.queryFeatures(queryString);
+  }
+  /**
+   * 根据图层,Graphic或Feature
+   * @author  liugh  20181209
+   * @param {*} view view
+   * @param {*} layer 图层
+   * @param {*} graphic  要高亮的要素
+   * @param {*} isGoto 是否跳转
+   */
+  function highlightByLayerGraphic(view, layer, graphic, isGoto) {
+    var highlightSelect = null;
+    view.whenLayerView(layer).then(function(layerView) {
+      if (highlightSelect) { highlightSelect.remove(); }
+      highlightSelect = layerView.highlight(graphic);
+    });
+    view.on("click", function (e) {
+      if (highlightSelect) { highlightSelect.remove(); }
+    });
+    if (isGoto) {
+      view.goTo(
+        {
+          target: graphic.geometry,
+          tilt: 70
+        },
+        {
+          duration: 2000,
+          easing: "in-out-expo"
+        }
+      );
+    }
+  }
+  var mapViewUtil = {getLayerByTitle: getLayerByTitle,getLayerByIndex: getLayerByIndex,getLayerById: getLayerById,setLayerVisible: setLayerVisible,highlightByLayerObjid: highlightByLayerObjid,queryFeathersFromLayer: queryFeathersFromLayer,queryFeathersFromLayer: queryFeathersFromLayer,highlightByLayerGraphic: highlightByLayerGraphic};
 
   /**
-   * ----------------此文件适合 二维通用方法 --------------------
    * 根据幻灯片的名称，切换到对应的视角
-   * @author  lee  20181206
+   * @author  lee 
    * @param {*} view  场景
    * @param {*} title  幻灯片的名称
    */
--- utils/view/map.js
+++ utils/view/map.js
@@ -1,3 +1,101 @@
-const mapViewUtil = {};
+/**
+ * 根据图层的title获取图层
+ * @author  lee  20181209
+ * @param {object} view  场景
+ * @param {string} title  名称
+ */
+function getLayerByTitle(view, title) {
+  const foundLayer = view.map.layers.find(lyr => {
+    return lyr.title === title;
+  });
+  return foundLayer;
+}
+/**
+ * @summary 根据图层的索引获取图层
+ * @description 适用范围：二、三维
+ * @author  lee  20181209
+ * @param {*} view  场景
+ * @param {*} index  图层索引
+ */
+function getLayerByIndex(view, index) {
+  const foundLayer = view.map.layers.getItemAt(index);
+  return foundLayer;
+}
+/**
+ * @summary 根据图层的id获取图层
+ * @author  lee  20181209
+ * @param {*} view  场景
+ * @param {*} id  图层id
+ */
+function getLayerById(view, index) {
+  const foundLayer = view.map.findLayerById();
+  return foundLayer;
+}
+/**
+ * 根据图层名称，控制图层显隐藏
+ * @author  lee  20181208
+ * @param {*} view  场景
+ * @param {*} title  名称
+ * @param {*} visible 显示/隐藏  true or false
+ */
+function setLayerVisible(view, title, visible) {
+  const foundLayer = getLayerByTitle(view, title);
+  foundLayer.visible = visible;
+}
+/**
+ * 根据要素的ObjectId高亮
+ * @author  lee  20181208
+ * @param {*} view  场景
+ * @param {*} title  名称
+ * @param {*} objectid 高亮要素的objectid
+ */
+function highlightByLayerObjid(view, title, objectid) {
+  const foundLayer = getLayerByTitle(view, title);
+  view.whenLayerView(foundLayer).then(view => {
+    view.highlight(objectid * 1);
+  });
+}
+/**
+ * 根据条件过滤要素图层中符合条件的要素
+ * @author  lee  20181209
+ * @param {*} layer  图层
+ * @param {*} queryWhere  查询条件
+ */
+function queryFeathersFromLayer(layer, queryWhere) {
+  const queryString = layer.createQuery();
+  queryString.where = queryWhere;
+  return layer.queryFeatures(queryString);
+}
+/**
+ * 根据图层,Graphic或Feature
+ * @author  liugh  20181209
+ * @param {*} view view
+ * @param {*} layer 图层
+ * @param {*} graphic  要高亮的要素
+ * @param {*} isGoto 是否跳转
+ */
+function highlightByLayerGraphic(view, layer, graphic, isGoto) {
+  let highlightSelect = null;
+  view.whenLayerView(layer).then(function(layerView) {
+    if (highlightSelect) highlightSelect.remove();
+    highlightSelect = layerView.highlight(graphic);
+  });
+  view.on("click", e => {
+    if (highlightSelect) highlightSelect.remove();
+  });
+  if (isGoto) {
+    view.goTo(
+      {
+        target: graphic.geometry,
+        tilt: 70
+      },
+      {
+        duration: 2000,
+        easing: "in-out-expo"
+      }
+    );
+  }
+}
+const mapViewUtil = {getLayerByTitle,getLayerByIndex,getLayerById,setLayerVisible,highlightByLayerObjid,queryFeathersFromLayer,queryFeathersFromLayer,highlightByLayerGraphic};
 
 export default mapViewUtil;
