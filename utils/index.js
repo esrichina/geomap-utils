5443cd855f708bc1baae40e3875cc142104cb422
yanwh <yanwh@esrichina.com.cn>
2018-12-24 17:28:32 +0800

    add folder struct

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,19 @@
-import { default as jsapi } from "./jsapi";
-import { default as view } from "./view";
+import { default as jsapi } from './jsapi';
+import { default as view } from './view';
+import { default as geometry } from './geometry';
+import { default as layer } from './layer';
+import { default as events } from './events';
 
 const utils = {
   jsapi,
-  view
+  view,
+  layer,
+  geometry,
+  events,
 };
 export default utils;
 
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
   // running in browser
   // inject the utils into window
   window.agsUtils = utils;
