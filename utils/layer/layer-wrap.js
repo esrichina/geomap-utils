3a6f6895c99cf36ae4b3122b834dd000b651c859
yanwh <yanwh@esrichina.com.cn>
2019-06-27 17:52:07 +0800

    添加防御性编码

--- utils/layer/layer-wrap.js
+++ utils/layer/layer-wrap.js
@@ -1,12 +1,23 @@
+import _ from 'lodash';
+
 /**
  * 受控的ArcGIS Layer类型，为Layer类包裹生命周期
  * 目的是使应用中的业务逻辑处理趋向统一
  */
 export default class BaseLayerWrap {
   constructor(opts) {
-    this.beforeOnLoad = opts.before;
-    this.afterUnload = opts.after;
+    if (!opts.layer) {
+      throw new Error('BaseLayerWrap: layer property must be specified!');
+    }
     this.layer = opts.layer;
+
+    if (_.isFunction(opts.before)) {
+      this.beforeOnLoad = opts.before;
+    }
+
+    if (_.isFunction(opts.after)) {
+      this.afterUnload = opts.after;
+    }
   }
 
   async beforeOnLoad() {}
